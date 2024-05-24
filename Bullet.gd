af1318b9395c2471ed2c33651f280e0c60ac6f0f
Robert Fleming <gnimelftrebor@gmail.com>
2024-05-24 13:39:58 -0700

    changed bullet code, added bullet timer

--- Bullet.gd
+++ Bullet.gd
@@ -1,8 +1,14 @@
 extends Area2D
+class_name Bullet
 
 export (int) var speed = 10
 
+onready var kill_timer = $KillTimer
+
 var direction := Vector2.ZERO
+
+func _ready() -> void:
+	kill_timer.start()
 
 func _physics_process(delta: float) -> void:
 	if direction != Vector2.ZERO:
@@ -12,3 +18,8 @@ func _physics_process(delta: float) -> void:
 
 func set_direction(direction: Vector2):
 	self.direction = direction
+	rotation += direction.angle()
+
+
+func _on_KillTimer_timeout():
+	queue_free()
--- World.gd
+++ World.gd
@@ -3,10 +3,13 @@ extends Node2D
 var borders = Rect2(1, 1, 38, 21)
 
 onready var tileMap = $TileMap
+onready var bullet_manager = $BulletManager
+onready var player = $Player
 
 func _ready():
 	randomize()
 	generate_level()
+	player.connect("player_fired_bullet", bullet_manager, "handle_bullet_spawned")
 
 func generate_level():
 	var walker = Walker.new(Vector2(19, 11), borders)
